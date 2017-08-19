902a90f917052c712c7fb1a9391a5c6244b35c58
Aaron Elkins <threcius@yahoo.com>
2017-08-19 14:35:39 +0800

    Vim write

--- OpenGL Template/OpenGL Template/main.cpp
+++ OpenGL Template/OpenGL Template/main.cpp
@@ -42,20 +42,20 @@ void initVertices(){
     GLuint VertexArrayID;
     glGenVertexArrays(1, &VertexArrayID);
     glBindVertexArray(VertexArrayID);
-    
+
     // Create and compile our GLSL program from the shaders
     program = LoadShaders(VERTEX_SHADER, FRAGMENT_SHADER);
-    
+
     static const GLfloat g_vertex_buffer_data[] = {
         -1.0f, -1.0f, 0.0f,
         1.0f, -1.0f, 0.0f,
         0.0f,  1.0f, 0.0f,
     };
-    
+
     glGenBuffers(1, &vertexbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
-    
+
 }
 // End Red book
 
@@ -93,7 +93,7 @@ bool gRender= true;
 bool init(){
     //Initialization flag
     bool success = true;
-    
+
     //Initialize SDL
     if(SDL_Init(SDL_INIT_VIDEO) < 0){
         printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
@@ -103,7 +103,7 @@ bool init(){
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
-        
+
         //Create window
         gWindow = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
         if(gWindow == NULL){
@@ -120,7 +120,7 @@ bool init(){
                 if(SDL_GL_SetSwapInterval( 1 ) < 0){
                     printf( "Warning: Unable to set VSync! SDL Error: %s\n", SDL_GetError());
                 }
-                
+
                 //Initialize OpenGL
                 if(!initGL()){
                     printf("Unable to initialize OpenGL!\n");
@@ -129,7 +129,7 @@ bool init(){
             }
         }
     }
-    
+
     return success;
 }
 
@@ -137,17 +137,17 @@ bool initGL()
 {
     bool success = true;
     GLenum error = GL_NO_ERROR;
-    
+
     //Initialize clear color
     glClearColor(0.9, 0.9, 0.9, 1);
-    
+
     //Check for error
     error = glGetError();
     if(error != GL_NO_ERROR){
         printf( "Error initializing OpenGL! %d\n", error);
         success = false;
     }
-    
+
     return success;
 }
 
@@ -168,12 +168,12 @@ void render(){
         glClear(GL_COLOR_BUFFER_BIT);
         return ;
     }
-    
+
     glClear(GL_COLOR_BUFFER_BIT);
-    
+
     // Use our shader
     glUseProgram(program);
-    
+
     // 1rst attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
@@ -185,10 +185,10 @@ void render(){
                           0,                  // stride
                           (void*)0            // array buffer offset
                           );
-    
+
     // Draw the triangle !
     glDrawArrays(GL_TRIANGLES, 0, 3); // 3 indices starting at 0 -> 1 triangle
-    
+
     glDisableVertexAttribArray(0);
 }
 
@@ -196,7 +196,7 @@ void close(){
     //Destroy window
     SDL_DestroyWindow(gWindow);
     gWindow = NULL;
-    
+
     //Quit SDL subsystems
     SDL_Quit();
 }
@@ -208,16 +208,16 @@ int main(int argc, char* args[]){
     }else{
         //Main loop flag
         bool quit = false;
-        
+
         //Event handler
         SDL_Event e;
-        
+
         //Enable text input
         SDL_StartTextInput();
-        
+
         // Vetext init
         initVertices();
-        
+
         //While application is running
         while(!quit){
             //Handle events on queue
@@ -233,20 +233,20 @@ int main(int argc, char* args[]){
                     handleKeys(e.text.text[ 0 ], x, y);
                 }
             }
-            
+
             //Render
             render();
-            
+
             //Update screen
             SDL_GL_SwapWindow(gWindow);
         }
-        
+
         //Disable text input
         SDL_StopTextInput();
     }
-    
+
     //Free resources and close SDL
     close();
-    
+
     return 0;
 }
