1e9d7b7e69c59476c8d405d3b9222ddd0c4da1c1
Aaron Elkins <threcius@yahoo.com>
2017-08-19 16:48:35 +0800

    Add glm

--- OpenGL Template/OpenGL Template/main.cpp
+++ OpenGL Template/OpenGL Template/main.cpp
@@ -11,7 +11,11 @@
 #include <errno.h>
 
 #include "GLUtil.h"
-
+#include <glm/vec3.hpp> // glm::vec3
+#include <glm/vec4.hpp> // glm::vec4
+#include <glm/mat4x4.hpp> // glm::mat4
+#include <glm/gtc/matrix_transform.hpp> // glm::translate, glm::rotate, glm::scale, glm::perspective
+#include <glm/gtc/constants.hpp> // glm::pi
 // Vertice shader
 const char * VERTEX_SHADER = R"SHADER(
 #version 330 core
